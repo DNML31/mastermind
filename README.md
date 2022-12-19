52809982b166292fa41168abf86fd872296693ea
DNML31 <danielchun31@gmail.com>
2022-12-19 14:22:41 -0800

    README update and compare method
    This commit updates the README with how I want my version of mastermind
    to be.
    
    Currently the code creates an object of Board class, creates the 4-digit
    code with new_code, and takes a user input after a prompt. The user
    input is turned into an array to compare with the class variable
    @@four_digit_code which was created in new_code method. The compare
    method was made to test comparison of the user input guess and the
    4-digit code class variable with an if..else block.
    
    Currently there aren't issues with comparing the two.

--- README.md
+++ README.md
@@ -10,3 +10,24 @@ guess was – whether it was exactly correct or just the correct color but in th
 
 Build a Mastermind game from the command line where you have 12 turns to guess 
 the secret code, starting with you guessing the computer’s random code.
+
+******
+
+*This version of mastermind will be closer to Wordle.
+
+4-digit code using numbers 1-6.
+Total of 12 guesses to get the correct 4-digit code.
+A clue is given after every guess to help figure out the code.
+The game is over if the code is matched or when turns run out.
+
+Clues are in XXXX format.
+	*An ‘X’ indicates a wrong number in that spot.
+	*An ‘O’ indicates a correct number in the correct spot.
+	*An ‘E’ indicates the number is correct BUT in the wrong spot.
+
+Example:
+
+If the 4-digit code is 1253 and the first guess is 1452…
+The clue is OXOE.
+If the second guess is 1245…
+The clue is OOXE.
--- mastermind.rb
+++ mastermind.rb
@@ -1,31 +1,50 @@
 class Board
 
   @@guesses = 0
+  @@four_digit_code = []
 
+  def initialize
+    puts "\nA new game has begun!"
+  end
+
+# changed new_code so that the class variable is still in array form to make
+# guess checking easier through iteration
   def new_code
-    @four_digit_code = four_digit_code = []
+    four_digit_code = []
     4.times do
       a = rand(1..6)
-      four_digit_code.push(a)
+      four_digit_code.push(a.to_i)
+      @@four_digit_code.push(a.to_i)
     end
-    @four_digit_code = four_digit_code.join
-    print @four_digit_code
+    print four_digit_code
+  end
+
+  def secret
+   @@four_digit_code
   end
 
   def guesses
-    puts "\n#{@@guesses}"
-  end
-
-  def initialize
-    @@guesses += 1
-    puts 'A new game has begun!'
+    puts "\nPlayer has guessed #{@@guesses} times."
   end
 
 end
 
+new_game = Board.new              # new game obj created
+new_code = new_game.new_code      # new 4-digit created (array)
 
-new_game = Board.new
-new_game.new_code
+puts "\ntake a guess"
+# this block takes user input and turns it into an array
+guess = gets.chomp
+guess = guess.split('')
+guess_array = guess.map(&:to_i)
 
-new_game.guesses
-# when a new game starts we need: 4-digit code, guess, clue, how many turns
+# x will be the secret code, y is the guess
+def compare(x, y)
+  if x == y
+    puts 'correct'
+  else
+    puts 'INCORRECT'
+  end
+end
+
+compare(new_game.secret, guess_array)
